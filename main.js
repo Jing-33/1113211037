f3ccd9a0a13cc5c7fa46278c73bf883d279ce082
Jing-33 <1113211037@gap.wzu.edu.tw>
2025-12-29 10:10:41 +0800

    final

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.scss';
+import 'bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// 初始化 AOS 動態效果
+AOS.init({
+  duration: 1000,
+  once: true,
+  offset: 100,
+});
+
+console.log('2026 Future Tech Forum Loaded!');
